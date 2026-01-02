f617e4811cccdf548f875dd960cee6baaefe722f
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-01-02 11:05:26 +0600

    first commit

---
+++ 1_Basics/hello.c
@@ -0,0 +1,10 @@
+/**
+ *    author:  SAGOR AHMED
+ *    created: 2 0 2 4       
+**/
+#include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
---
+++ 1_Basics/test.c
@@ -0,0 +1,12 @@
+/**
+ *    author:  SAGOR AHMED
+ *    created: 2 0 2 4       
+**/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n);
+    return 0;
+}
