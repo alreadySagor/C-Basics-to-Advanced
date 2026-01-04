ccf1b58a4e3166219b70ef2ea9154eb9615bbbac
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-01-04 14:57:15 +0600

    added all codes with extension .c

---
+++ 1_Basics/escape_sequence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is a line with a newline character\n");
+    printf("This is a line with a horizontal tab\tright here.\n");
+    printf("This is a string contains a \"double quote\"\n");
+    printf("This is a string contains a \'single quote\'\n");
+    printf("This is a string contains a \\backslash\\\n");
+    printf("This is a string contains a percent sign %%.\n");
+    return 0;
+}
